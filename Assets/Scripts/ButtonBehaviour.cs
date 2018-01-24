b2a655c0858ee7cc49225d7279d102decb24d26b
DanielSchoetzau <dani_schoetzau@gmx.de>
2018-01-24 17:34:38 +0100

    First Upload
    Es fehlt:
    Ausgang
    Sterben
    Enemy sterben
    Spawnpoints?

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour {
+
+    public void LoadLevelByIndex(int levelIndex)
+    {
+        Application.LoadLevel(1);
+    }
+
+	void Update () {
+        if (Input.anyKeyDown)
+            Application.LoadLevel(1);
+	}
+}
